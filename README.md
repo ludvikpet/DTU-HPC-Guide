5d057b02e817970c79b9bcea28c42bd4cd56ce86
ludvikpet <ludvikpet@gmail.com>
2026-01-20 01:09:42 +0100

    Created working main

---
+++ README.md
@@ -0,0 +1,14 @@
+# Welcome to a beginner friendly DTU HPC guide! 
+
+
+DTU HPC is DTUs computing cluster service, offering state of the art computing solution for developing computationally advanced algorithms. It is a more versatile alternative to services such as Google colab, as it allows to run experiments without connection and with control over the hardware used. This note is a hands-on tutorial on getting started on DTUs HPC system, and is, in most part, to be considered a practical extension of [DTU HPCs own material](https://www.hpc.dtu.dk/) for user's with little to no prior experience using cluster services or linux. We aditionally provide a GitHub repository which aids in establishing project structure and code debugging.   
+
+This tutorial has been made by Ludvík Petersen and Martin Ægidius.
+If you directly use the code provided, please cite us. 
+
+If you use the guide information, please remember to cite the [DTU DCC](https://www.hpc.dtu.dk/?page_id=4061).
+
+The step-by-step tutorial may be accessed [here](https://ludvikpet.github.io/DTU-HPC-Tutorial/)!
+
+
+
---
+++ scripts/add_machine_debug_config.sh
@@ -0,0 +1,40 @@
+: '
+    Shell script for adding an interactive node to users launch.json to enable debugging on interactive HPC nodes. 
+
+    Usage:
+        start a session on the specific interactive node and run the script. Enter the name of the machine.
+
+    Note: the script strips comments, and assumes valid json files (no trailing "," before {}-blocks)
+
+    Authors: Ludvík Petersen and Martin Ægidius, Technical University of Denmark 
+'
+#!/bin/bash 
+
+read -p "Enter interactive node name: " MACHINENAME
+echo "Fetching ip adress eth0..."
+IPAddr=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+echo "Appending to existing launch.json"
+
+# Strip comments
+grep -v '^\s*//' .vscode/launch.json > .vscode/launch_clean.json
+
+jq --arg ip "$IPAddr" --arg name "$MACHINENAME" '.configurations += [{
+    "name": ("Attach to debugger - $name"),
+    "type": "debugpy",
+    "request": "attach",
+    "connect": {
+        "host": $ip,
+        "port": 5678
+    },
+    "pathMappings": [{
+        "localRoot": "${workspaceFolder}",
+        "remoteRoot": "."
+    }],
+    "justMyCode": false
+}]' .vscode/launch_clean.json > .vscode/tmp.json
+
+echo "cleaning up..."
+mv .vscode/launch.json .vscode/launch.json.old 
+mv .vscode/tmp.json .vscode/launch.json
+rm -f .vscode/launch_clean.json  
+echo ".vscode/launch.json updated for $MACHINENAME. Old config saved to .vscode/launch.json.old"
